728f2786532caf7ab651aefb05ec53a67b85c7e8
sergey.tkachenko <serg.tkachenko@hotmail.com>
2020-02-02 20:43:42 +0200

    add time loading

--- src/components/Pivot.vue
+++ src/components/Pivot.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="md-layout md-alignment-center">
         <div class="md-layout-item md-size-75">
-            <h3>all records: {{allRecords}}, loaded records: {{loadedRecords}}</h3>
+            <h3>count records: {{allRecords}}, loaded: {{loadedMBytes}}MB</h3>
+            <h3>load time: {{loadingTime}}s, agg time: {{aggTime}}ms</h3>
             <vue-virtual-table :itemHeight="55"
                                :height="500"
                                :config="tableConfig"
@@ -42,8 +43,11 @@
             msg: String
         },
         data: () => ({
+            aggTime: 0,
+            loadingTime: 0,
             count: 0,
             loaded: 0,
+            loadedBytes: 0,
             users: [
                 {
                     id: 1,
@@ -103,11 +107,15 @@
             },
             loadedRecords: function () {
                 return this.loaded.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            },
+            loadedMBytes: function() {
+                return Math.round(this.loadedBytes / 1024 / 1024);
             }
         },
         methods: {
             transform(str) {
                 let data = str.split('[');
+                this.startLoadingAggTime = new Date();
                 data
                     .filter(x => x)
                     .forEach(d => {
@@ -115,13 +123,44 @@
                         this.loaded += batch.length;
                         this.table.update(batch);
                     });
+                this.calculateLoadingTime();
+                this.calculateAggTime();
+            },
+            calculateLoadingTime() {
+                this.endLoading = new Date();
+                this.loadingTime = Math.round((this.endLoading - this.startLoading) / 1000);
+            },
+            calculateAggTime() {
+                this.endLoading = new Date();
+                this.aggTime = Math.round((this.endLoading - this.startLoadingAggTime));
             },
             async fetchDataStream() {
+                this.startLoading = new Date();
                 fetch('/api/data-stream')
                     .then(async response => {
-                        const result = await response.text();
-                        this.transform(result);
-                    });
+                        const reader = response.body.getReader();
+                        let receivedLength = 0;
+                        let chunks = [];
+                        // eslint-disable-next-line no-constant-condition
+                        while(true) {
+                            const {done, value} = await reader.read();
+                            if (done) {
+                                break;
+                            }
+                            chunks.push(value);
+                            receivedLength += value.length;
+                            this.loadedBytes = receivedLength;
+                            this.calculateLoadingTime();
+                        }
+                        let chunksAll = new Uint8Array(receivedLength);
+                        let position = 0;
+                        for(let chunk of chunks) {
+                            chunksAll.set(chunk, position);
+                            position += chunk.length;
+                        }
+                        return new TextDecoder("utf-8").decode(chunksAll);
+                    })
+                    .then(async result => this.transform(result));
             },
             async fetchSchema() {
                 const response = await fetch(`/api/schema`);
@@ -168,6 +207,7 @@
             });
             view.on_update(async () => {
                 this.rows = await view.to_json();
+                this.calculateAggTime();
             });
             this.columns = ['__ROW_PATH__'].concat(await this.table.columns());
             this.rows = await view.to_json();
